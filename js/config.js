208dff62a4b082b86b788d90c79452bad80d3ae0
LaisvydasGi <laisvydas.giriunas@gmail.com>
2023-05-30 15:30:05 +0300

    refactored navigation

--- js/config.js
+++ js/config.js
@@ -2,17 +2,17 @@ export const API_URL = 'https://jsonplaceholder.typicode.com';
 export const POSTS_LIMIT = 20;
 export const ALBUMS_LIMIT = 10;
 export const PHOTOS_LIMIT = 20;
-// export const MENU_ITEMS = [
-//   {
-//     title: "users",
-//     path: "users.html",
-//   },
-//   {
-//     title: "posts",
-//     path: "posts.html",
-//   },
-//   {
-//     title: "albums",
-//     path: "albums.html",
-//   },
-// ];
+export const MENU_ITEMS = [
+  {
+    title: "users",
+    path: "users.html",
+  },
+  {
+    title: "posts",
+    path: "posts.html",
+  },
+  {
+    title: "albums",
+    path: "albums.html",
+  },
+];
